a1f0007587c325e5fa354a2d3f2626e34b480b1f
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-12-19 11:23:15 +0530

    designed the skeleton structure of header and body

--- App.js
+++ App.js
@@ -9,12 +9,74 @@ import ReactDOM from "react-dom/client";
  *  - Search
  *  - Restaurant Container
  *      - Restaurant Card
+ *          - Img
+ *          - Name of Res, Star Rating, Cuisine, delivery time
  * Footer
  *  - Copyright
  *  - Links
  *  - Contact Info
  */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="burger king"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"
+      />
+      <h3>Burger King</h3>
+      <h4>Burgers, Beverages and many more...</h4>
+      <h4>4.3 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
