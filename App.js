488eec5522f91fff7096f17fab1b370e2b8bfa37
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-12-19 10:32:15 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *      - Restaurant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Contact Info
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading />);
